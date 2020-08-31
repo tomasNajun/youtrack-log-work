a4da6a27850aa6219ec222c27636fc6a1ab44ad2
Tomas Najun <tomasnajun@gmail.com>
2020-08-31 19:00:14 -0300

    refactor: log row index

--- log-youtrack-work.sh
+++ log-youtrack-work.sh
@@ -7,27 +7,28 @@ AUTH_TOKEN="set your user token";
 YOUTRACK_HOST="https://company.myjetbrains.com";
 
 function logWork {
-	curl --location --request POST "${YOUTRACK_HOST}/youtrack/rest/issue/${issue}/timetracking/workitem" \
-		--header "Authorization: Bearer ${AUTH_TOKEN}" \
-		--header 'Content-Type: application/xml' \
-		--data-raw "<workItem>
-	    <date>$unix_date</date>
-	        <duration>$duration</duration>
-		    <description>$description</description>
-		        <worktype>
-			        <name>$workitem_type</name>
-				    </worktype>
-				    </workItem>"
+        curl --location --request POST "${YOUTRACK_HOST}/youtrack/rest/issue/${issue}/timetracking/workitem" \
+                --header "Authorization: Bearer ${AUTH_TOKEN}" \
+                --header 'Content-Type: application/xml' \
+                --data-raw "<workItem>
+            <date>$unix_date</date>
+                <duration>$duration</duration>
+                    <description>$description</description>
+                        <worktype>
+                                <name>$workitem_type</name>
+                                    </worktype>
+                                    </workItem>"
 }
 
-index=0
+x=1
 while IFS=, read -r date duration issue type description  
 do
-	unix_date=$(date "+%s%3N" -d ${date})
-	duration=$(echo ${duration} | sed 's/"//g')
-	issue=$(echo ${issue} | sed 's/"//g')
-	workitem_type=$(echo ${type} | sed 's/"//g')
-	description=$(echo ${description} | sed 's/"//g')
-	logWork
-	echo "Line: ${index}, Work '${description}' logged in issue ${issue} as '${workitem_type}'"
+        unix_date=$(date "+%s%3N" -d ${date})
+        duration=$(echo ${duration} | sed 's/"//g')
+        issue=$(echo ${issue} | sed 's/"//g')
+        workitem_type=$(echo ${type} | sed 's/"//g')
+        description=$(echo ${description} | sed 's/"//g')
+        logWork
+        echo "Row: '${x}', work logged in issue ${issue} as '${workitem_type}'"
+        x=$(( $x + 1 ))
 done < ${CSV_FILE}
